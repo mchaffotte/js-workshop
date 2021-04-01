681231be25dea29d93796541e9c07558a0f4fe65
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2021-04-01 23:25:01 +0200

    Create a company

--- express-sequelize/src/services/companies.js
+++ express-sequelize/src/services/companies.js
@@ -4,3 +4,13 @@ const Company = db.companies;
 exports.findAll = () => {
   return Company.findAll();
 };
+
+exports.create = (company) => {
+  if (!company.name) {
+    const emptyNameError = new Error("The company name cannot be empty");
+    emptyNameError.code = "ILLEGAL_ARGUMENT";
+    throw emptyNameError;
+  }
+  const { name } = company;
+  return Company.create({ name });
+};

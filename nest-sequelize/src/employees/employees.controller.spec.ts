d896ad32332a42e3dc941713be3111ff3ec616bf
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2021-04-11 15:14:37 +0200

    Mock sequelize for unit testing

--- nest-sequelize/src/employees/employees.controller.spec.ts
+++ nest-sequelize/src/employees/employees.controller.spec.ts
@@ -1,15 +1,24 @@
+import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { Employee } from './employee.model';
 import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
 
 describe('EmployeesController', () => {
   let controller: EmployeesController;
+  const mockModel = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EmployeesController],
-      providers: [EmployeesService],
+      providers: [
+        EmployeesService,
+        {
+          provide: getModelToken(Employee),
+          useValue: mockModel,
+        },
+      ],
     }).compile();
 
     controller = module.get<EmployeesController>(EmployeesController);
--- nest-sequelize/src/employees/employees.service.spec.ts
+++ nest-sequelize/src/employees/employees.service.spec.ts
@@ -1,12 +1,22 @@
+import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { Employee } from './employee.model';
 import { EmployeesService } from './employees.service';
 
 describe('EmployeesService', () => {
   let service: EmployeesService;
+  const mockModel = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmployeesService],
+      providers: [
+        EmployeesService,
+        {
+          provide: getModelToken(Employee),
+          useValue: mockModel,
+        },
+      ],
     }).compile();
 
     service = module.get<EmployeesService>(EmployeesService);

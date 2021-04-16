7c3b5f971d6c06a3a1dd07f3da0af93f55d438dd
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2021-04-16 23:36:53 +0200

    Add openAPI documentation

--- nest-sequelize/src/employees/employees.controller.ts
+++ nest-sequelize/src/employees/employees.controller.ts
@@ -10,12 +10,15 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { Employee } from './employee.model';
 import { TransactionInterceptor } from 'src/interceptors/TransactionInterceptor';
 
+@ApiTags('employees')
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -27,6 +30,7 @@ export class EmployeesController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get a pagniated list of employees' })
   findAll(
     @Query('offset') offset: number,
     @Query('limit') limit: number,
@@ -35,12 +39,19 @@ export class EmployeesController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get the employee based on its identifier' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: Employee,
+  })
   findOne(@Param('id', ParseIntPipe) id: number): Promise<Employee> {
     return this.employeesService.findOne(id);
   }
 
   @Patch(':id')
   @UseInterceptors(TransactionInterceptor)
+  @ApiOperation({ summary: 'Update the employee based on its identifier' })
   update(
     @Param('id') id: string,
     @Body() updateEmployeeDto: UpdateEmployeeDto,
@@ -50,6 +61,7 @@ export class EmployeesController {
 
   @Delete(':id')
   @UseInterceptors(TransactionInterceptor)
+  @ApiOperation({ summary: 'Delete the employee based on its identifier' })
   remove(@Param('id') id: string): Promise<void> {
     return this.employeesService.remove(+id);
   }
--- nest-sequelize/src/main.ts
+++ nest-sequelize/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Company example')
+    .setDescription('The company API description')
+    .setVersion('1.0')
+    .addTag('company')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, 'localhost');
   console.log(`Application is running on: ${await app.getUrl()}`);
-}
+};
 bootstrap();
